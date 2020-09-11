82a4cc6a6f191959a426314b56e7cc072e36d065
Nic <53277092+npm-nic@users.noreply.github.com>
2020-09-11 06:47:43 -0500

    DAY 4 (MVP)

---
+++ binary_search_tree/queue.py
@@ -0,0 +1,19 @@
+from singly_linked import LinkedList
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
---
+++ binary_search_tree/stack.py
@@ -0,0 +1,19 @@
+from singly_linked import LinkedList
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()
